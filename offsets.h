296e7adc23852418d9d8aaf6d8f09049ff8517e2
NeoTR <116913220+NeoTR@users.noreply.github.com>
2024-03-26 11:17:07 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,21 +1,21 @@
 #pragma once
-#define UWORLD 0x11E88988
+#define UWORLD 0x11E88988 // dsc.gg/OsmanHub
 #define GAME_INSTANCE 0x1d8
 #define LOCAL_PLAYERS 0x38
 #define PLAYER_CONTROLLER 0x30
-#define LOCAL_PAWN 0x338
+#define LOCAL_PAWN 0x338 // dsc.gg/OsmanHub
 #define PAWN_PRIVATE 0x308
 #define ROOT_COMPONENT 0x198
 #define RELATIVE_LOCATION 0x120
-#define PLAYER_STATE 0x2B0
+#define PLAYER_STATE 0x2B0 // dsc.gg/OsmanHub
 #define TEAM_INDEX 0x1251
 #define GAME_STATE 0x160
 #define PLAYER_ARRAY 0x2A8
-#define MESH 0x318
+#define MESH 0x318 // dsc.gg/OsmanHub
 #define COMPONENT_TO_WORLD 0x1c0
 #define BONE_ARRAY 0x598
-#define BONE_ARRAY_CACHE 0x658
+#define BONE_ARRAY_CACHE 0x658 // dsc.gg/OsmanHub
 #define LAST_SUMBIT_TIME 0x2E8
 #define LAST_SUMBIT_TIME_ON_SCREEN 0x2F0
 #define TIER = 0x13b
-#define WEAPONDATA = 0x4
+#define WEAPONDATA = 0x4 // dsc.gg/OsmanHub
